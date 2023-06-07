da3e1a5b39a100eb3bf3f43ae6ab4bba966640e7
barbara rzepka <bbrzepka@gmail.com>
2023-06-07 14:59:01 +0100

    upload files into google cloud

--- ecg_model/upload_cloud.py
+++ ecg_model/upload_cloud.py
@@ -3,20 +3,23 @@ import os
 from google.cloud import storage
 from ecg_model.params import *
 
-def upload_from_directory(storage_folder: str, folder_path: str):
+def list_gcp_files(storage_folder: str):
     client = storage.Client(project=GCP_PROJECT)
+    return [blob.name.split("/")[-1] for blob in client.list_blobs(BUCKET_NAME, prefix=storage_folder)]
+
+def upload_from_directory(storage_folder: str, folder_path: str, upload_type: str):
+    client = storage.Client(project=GCP_PROJECT)
+    if upload_type == "insert":
+        file_list = list_gcp_files(storage_folder)
 
     for _, _, files in os.walk(folder_path):
         for file in files:
-            upload_file(storage_folder, folder_path + "/" + str(file))
-            break
+            if file not in file_list:
+                upload_file(storage_folder, folder_path + "/" + str(file))
 
 def upload_file(storage_folder: str, file_path: str):
-    client = storage.Client()
+    client = storage.Client(project=GCP_PROJECT)
     bucket = client.bucket(BUCKET_NAME)
     file_name = file_path.split("/")[-1]
     blob = bucket.blob(f"{storage_folder}/{file_name}")
     blob.upload_from_filename(file_path)
-
-if __name__ == '__main__':
-    upload_from_directory("images","../raw_data/test")
