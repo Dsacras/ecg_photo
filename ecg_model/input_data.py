621f2c6647d7f8a1264833351ede5e03b239d7f8
barbara rzepka <bbrzepka@gmail.com>
2023-06-07 09:37:21 +0100

    update gitignore

--- ecg_model/input_data.py
+++ ecg_model/input_data.py
@@ -14,17 +14,20 @@ def read_report():
     df.to_csv('../raw_data')
 
 def read_data():
-    filepath="../raw_data/datfiles"
-    datfiles = [file.replace(".dat","") for file in listdir(filepath) if file.lower().endswith(('.dat'))]
+    filepath="../raw_data/records500"
+    imagepath="../raw_data/images/"
+    for subdir, dirs, files in os.walk(filepath):
+        for dir in dirs:
+            datfiles = [file.replace(".dat","") for file in listdir(filepath + "/" + dir) if file.lower().endswith(('.dat'))]
+            for file in datfiles:
+                record = wfdb.rdrecord(filepath +"/"+ dir +"/"+ file)
+                signal = record.p_signal.T
+                ecg_plot.plot(signal, sample_rate=record.fs, title="", show_lead_name=True, row_height=10,style="bw",
+                            show_grid=False, columns=2)
+                ecg_plot.save_as_jpg(file, imagepath)
 
-    path="../raw_data/images/"
-    for file in datfiles:
-        record = wfdb.rdrecord(filepath +"/"+ file)
-        signal = record.p_signal.T
-        ecg_plot.plot(signal, sample_rate=record.fs, title="", show_lead_name=True, row_height=10,style="bw",
-                      show_grid=False, columns=2)
-        ecg_plot.save_as_jpg(file, path)
-        break
+
+
     pass
 
 
