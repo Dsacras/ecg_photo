c6ce4bc200f99649eafc2d11520b8a821140f396
barbara rzepka <bbrzepka@gmail.com>
2023-06-12 12:30:55 +0100

    read files from gcp

--- ecg_model/params.py
+++ ecg_model/params.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 
 ##################  VARIABLES  ##################
-RAW_DATA_FOLDER = os.environ.get("RAW_DATA_FOLDER")
-EXPORTED_DATA_FOLDER = os.environ.get("EXPORTED_DATA_FOLDER")
+RAW_DATA_FOLDER=os.environ.get("RAW_DATA_FOLDER")
+EXPORTED_DATA_FOLDER=os.environ.get("EXPORTED_DATA_FOLDER")
 
-GCP_PROJECT = os.environ.get("GCP_PROJECT")
-GCP_REGION = os.environ.get("GCP_REGION")
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
+GCP_PROJECT=os.environ.get("GCP_PROJECT")
+GCP_REGION=os.environ.get("GCP_REGION")
+BUCKET_NAME=os.environ.get("BUCKET_NAME")
+GCP_IMG_FOLDER=os.environ.get("GCP_IMG_FOLDER")
