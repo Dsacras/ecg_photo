7da4c766c8cd54e401b1f8bf90659b9ad4971c37
Simone Mento <simo.me.29@gmail.com>
2023-06-14 17:38:47 +0100

    added Dockerfile

--- ecg_model/api/api.py
+++ ecg_model/api/api.py
@@ -7,7 +7,7 @@ import torch
 from io import BytesIO
 
 def load_model():
-    latest_model_path_to_save = "model_20230614-124525.pt"
+    latest_model_path_to_save = "ecg_model/api/model_20230614-124525.pt"
     latest_model = torch.load(latest_model_path_to_save)
     return latest_model
 
